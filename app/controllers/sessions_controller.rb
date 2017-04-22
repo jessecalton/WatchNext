34faf52e23a53f35bc5b05d79a77b7bf6f9fd606
Jesse Calton <jessecalton@gmail.com>
2017-04-21 17:51:48 -0700

    user can logout and when it logs in the page redirects to user page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
-  
+  include SessionsHelper
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user 
-      redirect_to root_path
+      redirect_to user_path(user)
     else
       flash[:notice] = "Invalid email/password combination"
       render 'new'
