8bfd62e79b6506cca3becb3a948782abb5886df4
Jesse Calton <jessecalton@gmail.com>
2017-04-23 15:37:09 -0700

    user can search and add movies to their list

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  include SessionsHelper
+
   def index
   end
 
@@ -6,6 +8,18 @@ class MoviesController < ApplicationController
   end
 
   def create
+    current_user
+    movie = params[:search][:title]
+    movie_json = MovieSearch.new(movie)
+    
+    if movie_json.response != "False" && current_user.movies != movie_json.movie_title
+      new_movie = Movie.create(title: movie_json.movie_title, user_id: current_user.id)
+
+      redirect_to user_path(current_user)
+    else
+      @errors = ["Please enter a valid movie title"]
+      render 'users#show'
+    end
   end
 
   def update
