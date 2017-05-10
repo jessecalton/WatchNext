fee5170fdf38b0c110191d8ab3c23f6b06e77eff
Jesse Calton <jessecalton@gmail.com>
2017-05-09 22:49:37 -0700

    changed routing to convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root    'users#index'
+  resources :users
+  resources :movies
+
   get     '/login',   to: 'sessions#new'
   post    '/login',   to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
   
-    resources :users
-    resources :movies
-    
+  root to:    'users#index'
 end
