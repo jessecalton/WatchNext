6a39337e5bf7c6614c5c5e10d7545dec7001efc1
Jesse Calton <jessecalton@gmail.com>
2017-06-01 17:54:35 -0700

    adds api key to OMDB API

--- app/models/movie_search.rb
+++ app/models/movie_search.rb
@@ -1,10 +1,11 @@
 class MovieSearch
   include HTTParty
-  base_uri 'http://www.omdbapi.com/?t='
+  # base_uri 'http://www.omdbapi.com/?t='
 
   def initialize(title)
+    api_key = ENV["API_KEY"]
     movie_url = title.gsub(/\s+/, "+")
-    uri = URI.parse("http://www.omdbapi.com/?t=#{movie_url}")
+    uri = URI.parse("http://www.omdbapi.com/?t=#{movie_url}&apikey=#{api_key}")
     uri.open {|f| @movie_data = self.class.get(uri) }
     
   end
