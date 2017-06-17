93b37319b5e0fda85f80b26055e46e2693c9eb06
Jesse Calton <jessecalton@gmail.com>
2017-06-16 18:22:44 -0700

    adds search bar for directors by iterating over collection. need to find a way to get rid of duplicate names

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ApplicationRecord
   belongs_to :user
 
+
+
   # scope :genre, -> (genre) { where("LOWER(genre) LIKE 'LOWER(?)'", genre) }
 
   # scope :director, -> (director) { where('LOWER(director) = LOWER(?)', director) }
