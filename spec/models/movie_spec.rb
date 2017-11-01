9613fc1033b692db05c370cca8597d1a08654fb5
Jesse Calton <jessecalton@gmail.com>
2017-10-31 23:27:16 -0700

    adds first two tests to movie attributes

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  let(:movie) { Movie.new(:title => )}
+  let(:movie) { Movie.new(:title => "WALL-E", 
+                          :runtime => 98, 
+                          :decade => "2000", 
+                          :genre => "Animation, Adventure, Family", 
+                          :director => "Andrew Stanton", 
+                          :actors => "Ben Burtt, Elissa Knight, Jeff Garlin, Fred Willard", 
+                          :awards => "Won 1 Oscar. Another 89 wins & 90 nominations.", 
+                          :poster_url => "https://images-na.ssl-images-amazon.com/images/M/MV5BMjExMTg5OTU0NF5BMl5BanBnXkFtZTcwMjMxMzMzMw@@._V1_SX300.jpg",
+                          :rotten_tomatoes => 96)}
 
-  pending 
+  describe 'attributes' do
+
+    it "has a title" do
+      expect(movie.title).to eq("WALL-E")
+    end
+
+    it "has a runtime that is an integer" do
+      expect(movie.runtime).to be_an(Integer)
+    end
+  end
 end
