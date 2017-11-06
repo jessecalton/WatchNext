e9d9236b2cc23c4a1ddce8d9a30f028e88678c20
Jesse Calton <jessecalton@gmail.com>
2017-11-05 23:24:53 -0800

    creates successful videotape for chinatown and hides api keys

--- spec/models/movie_search_spec.rb
+++ spec/models/movie_search_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe MovieSearch, type: :model do
   describe '#initialize' do
     it 'gets a response from the OMDB API' do
-      VCR.use_cassette 'vertigo' do
+      VCR.use_cassette 'chinatown' do
         api_key = ENV["API_KEY"]
 
-        @movie_data = JSON.load(open("http://www.omdbapi.com/?t=vertigo&apikey=#{<API_KEY>}"))
-        expect(@movie_data["Title"]).to eq("Vertigo")
+        @movie_data = JSON.load(open("http://www.omdbapi.com/?t=chinatown&apikey=#{api_key}"))
+        expect(@movie_data["Title"]).to eq("Chinatown")
       end
     end
-    it 'has a runtime saved as an integer' do
-      VCR.use_cassette 'vertigo' do
-        expect(self.movie_runtime).to eq(128)
-      end
-    end
+    # it 'has a runtime saved as an integer' do
+    #   VCR.use_cassette 'vertigo' do
+    #     expect(self.movie_runtime).to eq(128)
+    #   end
+    # end
   end
 end
