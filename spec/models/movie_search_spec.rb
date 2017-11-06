cb7cbdf708b3eb06812ed90ea10a432ef39fd496
Jesse Calton <jessecalton@gmail.com>
2017-11-05 23:54:41 -0800

    adds second moviesearch test for alice in wonderland films

--- spec/models/movie_search_spec.rb
+++ spec/models/movie_search_spec.rb
@@ -2,18 +2,28 @@ require 'rails_helper'
 
 RSpec.describe MovieSearch, type: :model do
   describe '#initialize' do
-    it 'gets a response from the OMDB API' do
-      VCR.use_cassette 'chinatown' do
-        api_key = ENV["API_KEY"]
-
-        @movie_data = JSON.load(open("http://www.omdbapi.com/?t=chinatown&apikey=#{api_key}"))
-        expect(@movie_data["Title"]).to eq("Chinatown")
+    context 'search by movie title' do
+      it 'gets a response from the OMDB API when given a movie title' do
+        VCR.use_cassette('chinatown', :record => :all) do
+          new_movie = MovieSearch.new("chinatown","","")
+          expect(new_movie.movie_title).to eq("Chinatown")
+        end
       end
     end
-    # it 'has a runtime saved as an integer' do
-    #   VCR.use_cassette 'vertigo' do
-    #     expect(self.movie_runtime).to eq(128)
-    #   end
-    # end
+    context 'two movies with same title' do
+      it 'search with title and year to get specific film' do
+        VCR.use_cassette('alice', :record => :all) do
+          new_movie = MovieSearch.new("alice in wonderland","1951","")
+          expect(new_movie.movie_title).to eq("Alice in Wonderland")
+          expect(new_movie.movie_decade).to eq(1950)
+        end
+        VCR.use_cassette('alice2010', :record => :all) do
+          new_movie = MovieSearch.new("alice in wonderland","2010","")
+          expect(new_movie.movie_title).to eq("Alice in Wonderland")
+          expect(new_movie.movie_decade).to eq(2010)
+          expect(new_movie.movie_director).to eq("Tim Burton")
+        end
+      end
+    end
   end
 end
