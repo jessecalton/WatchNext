079c03159edaadf9f9b0f1f122357111a7317aac
jessecalton <jessecalton@gmail.com>
2017-11-07 22:28:48 -0800

    finishes writing moviesearch model tests, including ones for the response

--- spec/models/movie_search_spec.rb
+++ spec/models/movie_search_spec.rb
@@ -3,7 +3,17 @@ require 'rails_helper'
 RSpec.describe MovieSearch, type: :model do
   describe '#initialize' do
     context 'search by movie title' do
-      it 'gets a response from the OMDB API when given a movie title' do
+      it 'returns a response of true from the OMDB API if a valid call is made' do
+        VCR.use_cassette('chinatown', :record => :once) do
+          new_movie = MovieSearch.new("chinatown","","")
+          expect(new_movie.response).to eq("True")
+        end
+        VCR.use_cassette('bad_movie', :record => :once) do
+          new_movie = MovieSearch.new("qwerty123","","")
+          expect(new_movie.response).to eq("False")
+        end
+      end
+      it 'calls #movie_title to get the title of the film' do
         VCR.use_cassette('chinatown', :record => :once) do
           new_movie = MovieSearch.new("chinatown","","")
           expect(new_movie.movie_title).to eq("Chinatown")
@@ -50,6 +60,13 @@ RSpec.describe MovieSearch, type: :model do
           expect(new_movie.movie_poster).to include(".jpg")
         end
       end
+      it 'calls #rotten_tomatoes_score to get an integer of the film\'s RottenTomatoes.com score' do
+        VCR.use_cassette('chinatown', :record => :once) do
+          new_movie = MovieSearch.new("chinatown","","")
+          expect(new_movie.rotten_tomatoes_score).to be_an(Integer)
+          expect(new_movie.rotten_tomatoes_score).to be(98)
+        end
+      end
     end
     context 'advanced search' do
       it 'search with title and year to get specific film' do
@@ -57,12 +74,14 @@ RSpec.describe MovieSearch, type: :model do
           new_movie = MovieSearch.new("alice in wonderland","1951","")
           expect(new_movie.movie_title).to eq("Alice in Wonderland")
           expect(new_movie.movie_decade).to eq(1950)
+          expect(new_movie.response).to eq("True")
         end
         VCR.use_cassette('alice2010', :record => :once) do
           new_movie = MovieSearch.new("alice in wonderland","2010","")
           expect(new_movie.movie_title).to eq("Alice in Wonderland")
           expect(new_movie.movie_decade).to eq(2010)
           expect(new_movie.movie_director).to eq("Tim Burton")
+          expect(new_movie.response).to eq("True")
         end
       end
       it 'searches with a film\'s IMDB ID number' do
@@ -70,6 +89,7 @@ RSpec.describe MovieSearch, type: :model do
           new_movie = MovieSearch.new("","","tt0245429")
           expect(new_movie.movie_title).to eq("Spirited Away")
           expect(new_movie.movie_decade).to eq(2000)
+          expect(new_movie.response).to eq("True")
         end
       end
     end
