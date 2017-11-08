896a104e42403beb3e95fa05a5b9a418fb850a36
Jesse Calton <jessecalton@gmail.com>
2017-11-08 15:06:01 -0800

    adds users controller spec and passes first test

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+  describe 'GET index' do
+    it 'has a 200 status code' do
+      get :index
+
+      expect(response.status).to eq(200)
+    end
+  end
+end
