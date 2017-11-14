19d7f8cb785c355bdc8ff91200840c85e2274e8b
Jesse Calton <jessecalton@gmail.com>
2017-11-14 00:12:01 -0800

    adds test for advanced search in user feature spec

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -15,4 +15,18 @@ RSpec.feature "Users", type: :feature do
       expect(page).to have_content("Fargo")
     end
   end
+  describe 'user adds a movie using advanced search' do
+    it 'updates to the page asynchronously' do
+      login(@user)
+      find('p', :text => 'Advanced Search').click
+      
+      VCR.use_cassette('jedi', :record => :once) do
+          fill_in 'adv_search_title', with: "star wars"
+          fill_in 'adv_search_year', with: "1983"
+          within(:css, 'form.search-form.advanced') { click_button "Add Movie" }
+      end
+      expect(page).to have_content("Return of the Jedi")
+      # save_and_open_page
+    end
+  end
 end
